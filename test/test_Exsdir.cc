1b0a22e323c3a272f21533813c4d283d38f42e34
Mark G. Gray <gray@lanl.gov>
2013-05-08 13:47:04 -0600

    Reorganized build around Autotools

--- test/test_Exsdir.cc
+++ test/test_Exsdir.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Library.hh"
+#include "Exsdir.hh"
 
 int main(int argc, char *argv[])
 {
@@ -10,22 +10,22 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  ndatk::Library x(argv[1]);     // parse filename
+  ndatk::Exsdir x(argv[1]);     // parse filename
 
   // Print file header
-  std::cout << x.get(ndatk::Library::string_val::NAME) << std::endl
-            << x.get(ndatk::Library::string_val::DATE) << std::endl
-            << x.get(ndatk::Library::string_val::INFO) << std::endl;
+  std::cout << x.get(ndatk::Exsdir::string_val::NAME) << std::endl
+            << x.get(ndatk::Exsdir::string_val::DATE) << std::endl
+            << x.get(ndatk::Exsdir::string_val::INFO) << std::endl;
 
   //Print first 10 entries
 
   for (int i = 0; i != 10; i++) {
-    std::string id = x.get(ndatk::Library::string_val_n::ID, i);
+    std::string id = x.get(ndatk::Exsdir::string_val_n::ID, i);
     std::cout << id << ":"
-              << " " << x.get(ndatk::Library::string_val_x::NAME, id)
-              << " " << x.get(ndatk::Library::int_val_x::ADDRESS, id)
-              << " " << x.get(ndatk::Library::float_val_x::AT_WGT, id)
-              << " " << x.get(ndatk::Library::float_val_x::TEMP, id)
+              << " " << x.get(ndatk::Exsdir::string_val_x::NAME, id)
+              << " " << x.get(ndatk::Exsdir::int_val_x::ADDRESS, id)
+              << " " << x.get(ndatk::Exsdir::float_val_x::AT_WGT, id)
+              << " " << x.get(ndatk::Exsdir::float_val_x::TEMP, id)
               << std::endl;
   }
   return 0;
