119307104cfe7083ae307380a04c019746606d5b
Tim Burke <tpburke@lanl.gov>
2020-07-21 12:35:14 -0600

    Updates to installation
    * Changes environment variable name from NDATK_DATA_PATH to NDATK_DATAPATH
    * Installs an exsdir file that is created based upon the available data in NDATK_DATAPATH or the default data paths if NDATK_DATAPATH is not set
    * Installs a symlink in CMAKE_PREFIX_PATH to enable non-standard installation locations (e.g. spack installations, personal desktop installations)

--- cmake/InstallLibFiles.cmake
+++ cmake/InstallLibFiles.cmake
@@ -1,8 +1,8 @@
 # NDTAK has three hard-coded options for data paths, depending on if you're on HPC, XLAN, or LLNL machines.
-# This function checks those three paths to determine which one is valid and sets NDATK_DATA_PATH cmake variable
+# This function checks those three paths to determine which one is valid and sets NDATK_DATAPATH cmake variable
 function(install_lib_files)
-  if (DEFINED ENV{NDATK_DATA_PATH})
-    set(NDATK_DATA_PATH $ENV{NDATK_DATA_PATH})
+  if (DEFINED ENV{NDATK_DATAPATH})
+    set(NDATK_DATAPATH $ENV{NDATK_DATAPATH})
   else()
     set(searchPaths 
       "/opt/local/codes/data/nuclear/ndatk/data" 
@@ -11,15 +11,15 @@ function(install_lib_files)
     )
     foreach(path ${searchPaths})
       if (EXISTS ${path})
-        set(NDATK_DATA_PATH ${path})
+        set(NDATK_DATAPATH ${path})
       endif()
     endforeach()
   endif()
 
-  if (DEFINED NDATK_DATA_PATH)
-    message("ndatk InstallLibFiles -- using NDATK_DATA_PATH: ${NDATK_DATA_PATH}")
+  if (DEFINED NDATK_DATAPATH)
+    message("ndatk InstallLibFiles -- using NDATK_DATAPATH: ${NDATK_DATAPATH}")
   else()
-    message(WARNING "ndatk -- unable to find data.  Set NDATK_DATA_PATH environment variable to location of data.")
+    message(WARNING "ndatk -- unable to find data.  Set NDATK_DATAPATH environment variable to location of data.")
   endif()
 
   set(libraries 
@@ -33,7 +33,7 @@ function(install_lib_files)
   )
 
   foreach(library ${libraries})
-    if (EXISTS "${NDATK_DATA_PATH}/${library}")
+    if (EXISTS "${NDATK_DATAPATH}/${library}")
       INSTALL(FILES "${CMAKE_CURRENT_SOURCE_DIR}/cmake/lib_files/${library}.lib"
         DESTINATION ${CMAKE_INSTALL_PREFIX}
         PERMISSIONS OWNER_READ OWNER_WRITE
@@ -42,15 +42,14 @@ function(install_lib_files)
     endif()
   endforeach()
 
-# install exsdir file - TODO: configure exsdir based on installed libraries
-  INSTALL(FILES "${CMAKE_CURRENT_SOURCE_DIR}/cmake/lib_files/exsdir"
-        DESTINATION ${CMAKE_INSTALL_PREFIX}
-        PERMISSIONS OWNER_READ OWNER_WRITE
-                    GROUP_READ
-                    WORLD_READ)
+# install exsdir file based on libraries available in NDATK_DATAPATH
+  include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CreateExsdirFile.cmake)
+  create_exsdir_file()
 
   if (EXISTS "${CMAKE_INSTALL_PREFIX}/data")
   else()
-    INSTALL(CODE "execute_process(COMMAND cmake -E create_symlink ${NDATK_DATA_PATH} ${CMAKE_INSTALL_PREFIX}/data)")
+    INSTALL(CODE "execute_process(COMMAND cmake -E create_symlink ${NDATK_DATAPATH} ${CMAKE_INSTALL_PREFIX}/data)")
   endif()
+
+
 endfunction()
